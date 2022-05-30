9b91d9d4a38e3b0544ecd3d9bd111f9e49998f1d
KarachunVladislav01 <karachunvladislav@gmail.com>
2022-05-30 13:01:59 +0300

    fix: modal menu close after redirect, added cursor pointer

--- src/components/ModalMenu/Item/index.tsx
+++ src/components/ModalMenu/Item/index.tsx
@@ -4,16 +4,22 @@ import { FC } from "react";
 
 import { useLocation, useNavigate } from "react-router-dom";
 import { SCREENS } from "../../../navigation/constants";
+import { useModalMenuContext } from "../../../context/useModalMenuContext";
 
 const Item: FC<{ link: SCREENS; title: string }> = ({ link, title }) => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const navigateTo = () => {
-    navigate(link);
-  };
+  const { setIsVisible } = useModalMenuContext();
 
   const isCurrentPage = () => location.pathname === link;
+
+  const navigateTo = () => {
+    if (isCurrentPage()) return;
+
+    setIsVisible(false);
+    navigate(link);
+  };
 
   return (
     <li
