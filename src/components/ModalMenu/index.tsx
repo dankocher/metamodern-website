17981ee6f8c79072c645ad63b6bdfebce8417ed8
KarachunVladislav01 <karachunvladislav@gmail.com>
2022-06-03 15:57:17 +0300

    fix: modal menu cetred

--- src/components/ModalMenu/index.tsx
+++ src/components/ModalMenu/index.tsx
@@ -8,16 +8,10 @@ import { SCREENS } from "../../navigation/constants";
 
 import Item from "./Item";
 
-import { IconButton, IconLink } from "../ActiveIcon";
-
-import logo from "../../assets/images/logo.png";
-import close from "../../assets/images/close.png";
 import translation from "../../i18n/en.json";
 
 const ModalMenu = () => {
-  const { isVisible, setIsVisible } = useModalMenuContext();
-
-  const openModalMenu = () => setIsVisible(false);
+  const { isVisible } = useModalMenuContext();
 
   useEffect(() => {
     document.getElementsByTagName("html")[0].style.overflowY = isVisible
@@ -27,11 +21,6 @@ const ModalMenu = () => {
 
   return (
     <div style={{ left: isVisible ? 0 : "200%" }} className={styles.container}>
-      {/* <div className={styles.header}>
-        <IconLink icon={logo} link={SCREENS.HOME} />
-
-        <IconButton icon={close} onClick={openModalMenu} />
-      </div> */}
       <ul className="gilroyBlack82">
         <Item link={SCREENS.PORTFOLIO} title={translation.portfolio} />
         <Item link={SCREENS.ABOUT_US} title={translation.aboutUs} />
