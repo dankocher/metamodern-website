700c2b3ea1f708111432ffb85b2a8477053ead4f
KarachunVladislav01 <karachunvladislav@gmail.com>
2022-07-27 16:15:39 +0300

    feat: added noize to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import Navigation from "./navigation";
+import { useEffect } from 'react';
 
-import { ModalMenuProvider } from "./context/useModalMenuContext";
+import Navigation from './navigation';
+
+import { ModalMenuProvider } from './context/useModalMenuContext';
+
+import grained from './utils/grained';
 
 function App() {
+  useEffect(() => {
+    window.document.getElementsByTagName('html')[0].id = 'container-noise';
+
+    grained('#container-noise', {
+      animate: true,
+      patternWidth: 200,
+      patternHeight: 200,
+      grainOpacity: 0.04,
+      grainDensity: 2,
+      grainWidth: 2,
+      grainHeight: 2,
+      grainChaos: 0.5,
+      grainSpeed: 5,
+    });
+  }, []);
+
   return (
     <ModalMenuProvider>
       <Navigation />
