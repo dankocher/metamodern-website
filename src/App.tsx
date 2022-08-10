7c4847dab1e9b2d628f156d16081f0fd79a31b17
KarachunVladislav01 <karachunvladislav@gmail.com>
2022-08-10 11:13:42 +0300

    fix: deleted noize

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import { useEffect } from 'react';
-
 import Navigation from './navigation';
 
 import { ModalMenuProvider } from './context/useModalMenuContext';
 
-import grained from './utils/grained';
-
 function App() {
-  useEffect(() => {
-    window.document.getElementsByTagName('html')[0].id = 'container-noise';
-
-    grained('#container-noise', {
-      animate: true,
-      patternWidth: 200,
-      patternHeight: 200,
-      grainOpacity: 0.04,
-      grainDensity: 2,
-      grainWidth: 2,
-      grainHeight: 2,
-      grainChaos: 0.5,
-      grainSpeed: 5,
-    });
-  }, []);
-
   return (
     <ModalMenuProvider>
       <Navigation />
