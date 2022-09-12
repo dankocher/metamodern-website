a2f3b51e7e2881aa6c44effa68ebfcb9407972da
ivan <severinec7788@mail.ru>
2022-09-12 17:59:38 +0300

    fix: footer at the bottom

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,5 +1,6 @@
 import Footer from '../Footer';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import styles from './index.module.scss';
 import ModalMenu from '../ModalMenu';
 import { motion } from 'framer-motion';
 import { useModalMenuContext } from '../../context/useModalMenuContext';
@@ -7,17 +8,34 @@ import { useModalMenuContext } from '../../context/useModalMenuContext';
 const PageWrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
   const { isVisible } = useModalMenuContext();
   const [duration, setDuration] = useState(0.2);
+  const wrapperRef = useRef(null);
 
   useEffect(() => {
     isVisible ? setDuration(0) : setDuration(0.2);
   }, [isVisible]);
 
+  useEffect(() => {
+    if (
+      document.documentElement.clientHeight > wrapperRef.current.clientHeight
+    ) {
+      document.documentElement.style.setProperty(
+        '--element-height',
+        100 + 'vh'
+      );
+    }
+  }, [document.documentElement.clientHeight]);
+
   return (
     <>
-      <motion.div exit={{ opacity: 0 }} transition={{ duration: duration }}>
+      <motion.div
+        exit={{ opacity: 0 }}
+        transition={{ duration: duration }}
+        className={styles.page}
+        ref={wrapperRef}
+      >
         {children}
-        <Footer />
       </motion.div>
+      <Footer />
     </>
   );
 };
